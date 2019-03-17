bb258d9498fa420acceef66f24a365ebca7f7383
Jacob Ingalls <jingalls@uoregon.edu>
2019-03-17 01:45:15 -0700

    Updates

---
+++ precache-manifest.612d450fe1019b63bb5360c25041359c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "442e02b39db9cf6b5cab",
+    "url": "/nbareact2/build/static/js/runtime~main.86f57f44.js"
+  },
+  {
+    "revision": "9f4b986e8d19479759b9",
+    "url": "/nbareact2/build/static/js/main.0e07432d.chunk.js"
+  },
+  {
+    "revision": "e93bc7dff72ddddbd85a",
+    "url": "/nbareact2/build/static/js/2.a81b9cc6.chunk.js"
+  },
+  {
+    "revision": "9f4b986e8d19479759b9",
+    "url": "/nbareact2/build/static/css/main.a8bd7f6b.chunk.css"
+  },
+  {
+    "revision": "71e9dfced2f8ac573d7ce07138703a66",
+    "url": "/nbareact2/build/index.html"
+  }
+];
