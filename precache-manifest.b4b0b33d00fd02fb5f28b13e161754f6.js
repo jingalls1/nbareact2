c2550491628e5991fd607ac03cf9738a3c58c2d3
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-13 13:27:14 -0700

    Updates

---
+++ precache-manifest.b4b0b33d00fd02fb5f28b13e161754f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "696af556306fa07dc382",
+    "url": "/nbareact2/static/js/runtime~main.29b75276.js"
+  },
+  {
+    "revision": "65df2bc78f3e73cfd1a7",
+    "url": "/nbareact2/static/js/main.1785c89a.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/nbareact2/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "65df2bc78f3e73cfd1a7",
+    "url": "/nbareact2/static/css/main.4a460495.chunk.css"
+  },
+  {
+    "revision": "603b6539b208c6bc3565e7cfde26d4e8",
+    "url": "/nbareact2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nbareact2/precache-manifest.28b99a44358c3d6d0daaac8470cf912b.js"
+  "/nbareact2/precache-manifest.b4b0b33d00fd02fb5f28b13e161754f6.js"
 );
 
 workbox.clientsClaim();
